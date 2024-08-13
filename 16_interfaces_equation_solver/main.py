2468cecf49f239876ba2aa6f34c9c50a6d28559d
mrwks11 <69803244+chungungo11@users.noreply.github.com>
2024-08-13 09:06:30 +0200

    added code until step 17

--- 16_interfaces_equation_solver/main.py
+++ 16_interfaces_equation_solver/main.py
@@ -7,8 +7,12 @@ class Equation(ABC):
     def __init__(self, *args):
         if (self.degree + 1) != len(args):
             raise TypeError(
-                f"'{self.__class__.__name__}' object takes {self.degree + 1} positional arguments but {len(args)} were given"
+                f"'Equation' object takes {self.degree + 1} positional arguments but {len(args)} were given"
             )
+        if any(not isinstance(arg, (int, float)) for arg in args):
+            raise TypeError("Coefficients must be of type 'int' or 'float'")
+        if args[0] == 0:
+            raise ValueError("Highest degree coefficient must be different from zero")
         
     def __init_subclass__(cls):
         if not hasattr(cls, "degree"):
